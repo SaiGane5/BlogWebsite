8f0886ed0e0ec34de01674ff60587b103c74be82
SaiGane5 <128854913+SaiGane5@users.noreply.github.com>
2024-06-20 12:45:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,40 +35,39 @@ let posts = [
 let lastId = 3;
 
 // Middleware
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-//Write your code here//
-
-//CHALLENGE 1: GET All posts
-app.get("/posts",(req,res)=>{
+// GET all posts
+app.get("/posts", (req, res) => {
+  console.log(posts);
   res.json(posts);
 });
 
-//CHALLENGE 2: GET a specific post by id
-app.get("/posts/:id",(req,res)=>{
+// GET a specific post by id
+app.get("/posts/:id", (req, res) => {
   const post = posts.find((p) => p.id === parseInt(req.params.id));
   if (!post) return res.status(404).json({ message: "Post not found" });
   res.json(post);
 });
 
-//CHALLENGE 3: POST a new post
-app.post("/posts",(req,res)=>{
-  const newId = lastId+1;
-  const newPost = {
+// POST a new post
+app.post("/posts", (req, res) => {
+  const newId = lastId += 1;
+  const post = {
     id: newId,
     title: req.body.title,
     content: req.body.content,
     author: req.body.author,
     date: new Date(),
-  }
+  };
   lastId = newId;
-  posts.push(newPost);
-  res.status(201).json(posts);
+  posts.push(post);
+  res.status(201).json(post);
 });
 
-//CHALLENGE 4: PATCH a post when you just want to update one parameter
-app.patch("/posts/:id",(req,res)=>{
+// PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req, res) => {
   const post = posts.find((p) => p.id === parseInt(req.params.id));
   if (!post) return res.status(404).json({ message: "Post not found" });
 
@@ -79,8 +78,8 @@ app.patch("/posts/:id",(req,res)=>{
   res.json(post);
 });
 
-//CHALLENGE 5: DELETE a specific post by providing the post id.
-app.delete("/posts/:id",(req,res)=>{
+// DELETE a specific post by providing the post id
+app.delete("/posts/:id", (req, res) => {
   const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
   if (index === -1) return res.status(404).json({ message: "Post not found" });
 
